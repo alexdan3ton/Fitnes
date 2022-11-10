2964870e035214d474786a4d2d8ddfb692026e00
Aleksey <3ton1995@mail.ru>
2022-11-10 18:59:38 +0300

    delete scripttest

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+// Урок 2 ClassList и делегирование событий
+
+// .className показывает все классы
+// .сlassList позволяет работать с классами узла
+// .length возвращает кол-во классов
+// .item(0) показывает класс по индексу
+// .add() добавляет класс
+// .remove() удаляет класс
+// .toggle() либо добавляет класс, либо удаляет, если он уже есть
+
+// // querySelectorAll Возвращает NodeList, содержащий
+// // все подходящие узлы.
+// const btns = document.querySelectorAll("button"),
+//   wrapper = document.querySelector(".btn-block");
+
+// btns[1].classList.add("button", "red");
+
+//Добавляем обработчик событий на кнопку btns[0],
+// при нажатии, проверяет, есть ли у btns[1] класс 'red',
+// если нет удаляет, если есть - добавляет
+// btns[0].addEventListener("click", () => {
+//   if (!btns[1].classList.contains("red")) {
+//     btns[1].classList.add("red");
+//   } else {
+//     btns[1].classList.remove("red");
+//   }
+//   // либо так
+//   //   btns[1].classList.toggle('red');
+// });
+
+//Проверяем есть ли класс 'red' у элемента btns[1]
+// if (btns[1].classList.contains("red")) {
+//   console.log(true);
+// }
+
+//event содержит в себе
+//всю информацию, о том элементе, в котором происходит событие
+// .tagName
+// Делегируем событие с родителя wrapper на его потомка с классом blue
+// //Продвинутое делегирование от GOOGLE event.target.matches("button.blue")
+// wrapper.addEventListener("click", (event) => {
+//   if (event.target && event.target.matches("button.blue")) {
+//     console.log("hello");
+//   }
+// });
+
+//Перебирает все кнопки bnts и вешает на них обработчик событий,
+// !!! НО НЕ ВЕШАЕТ ЕГО НА ДИНАМИЧЕСКИ ДОБАВЛЕННЫЕ ЭЛЕМЕНТЫ
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello')
+//     })
+// })
+
+//.createElement создает элемент на странице
+// //потом добавляем класс 'red' к кнопе и добавляем кнопку в родитель wrapper
+// const btn = document.createElement("button");
+// btn.classList.add("blue");
+// wrapper.append(btn);
+
 // const tabsParent = document.querySelector(".buttons");
 
 const initialTab = document.querySelectorAll(".btn");
